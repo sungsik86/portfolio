d6908be7a0338950e10de0f409ea4bd20195df1a
sungsik <sungsik@sungsikui-MacBookPro.local>
2026-03-10 15:41:02 +0900

    feat: add mobile TWD to KRW trans converter with auto exchange rate

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("projects/flyio-django-deploy/", views.flyio_deploy, name="flyio_deploy"),
+    path("projects/trans-converter/", views.trans_converter, name="trans_converter"),
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "portfolio/index.html")
+
+
+def flyio_deploy(request):
+    return render(request, "portfolio/flyio_deploy.html")
+
+
+def trans_converter(request):
+    return render(request, "portfolio/trans_converter.html")
